a1f34960b81d87038b1e31ac52e48cf812989541
meir <contact@flamingo.dev>
2025-06-19 13:37:14 +0200

    add builtin format function

---
+++ src/ast/builtin/format.rs
@@ -0,0 +1,33 @@
+use crate::ast::environment::{Scope, Value};
+use crate::ast::statement::Statement;
+use crate::context::Context;
+
+pub fn builtin_format(
+    ctx: &Context,
+    _: &Vec<Statement>,
+    inputs: &Vec<Value>,
+    scope: &mut Scope,
+) -> Value {
+    if inputs.len() == 0 {
+        panic!(
+            "Expected atleast one argument for 'format', got {}",
+            inputs.len()
+        );
+    }
+
+    match &inputs[0] {
+        Value::Str(src) => {
+            let mut src = src.clone();
+            for i in 1..inputs.len() {
+                let value = inputs.get(i).unwrap();
+                let str = value.render(ctx, scope);
+                src = src.replacen("{}", str.as_str(), 1);
+            }
+            Value::Str(src)
+        }
+        _ => panic!(
+            "Expected a string argument for 'format', got {}",
+            inputs[0].get_type()
+        ),
+    }
+}
--- src/ast/builtin/mod.rs
+++ src/ast/builtin/mod.rs
@@ -2,11 +2,14 @@ use super::environment::{Type, Value};
 use crate::resolver::File;
 use import::builtin_use;
 
+mod format;
 mod import;
 
 pub fn init(file: &mut File) {
     file.environment
         .define_builtin_function("use".into(), builtin_use, Type::Any);
+    file.environment
+        .define_builtin_function("format".into(), format::builtin_format, Type::Str);
     file.environment.define_builtin_function(
         "hello_world".into(),
         |_, _, _, _| Value::Str("Hello world!".into()),
