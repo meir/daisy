9b711599b9730b2008a07ca6a5f3305def544e0f
meir <contact@flamingo.dev>
2025-07-25 10:06:56 +0200

    remove debug log

--- src/ast/statement/collect.rs
+++ src/ast/statement/collect.rs
@@ -4,8 +4,6 @@ use crate::ast::expression::Expression;
 pub fn collect(expression: Expression) -> Statement {
     Box::new(move |ctx, scope| {
         let value = expression(ctx, scope);
-        println!("Collecting value: {}", value);
-
         Result::Collect(vec![value])
     })
 }
