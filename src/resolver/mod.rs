ba879ca084b5a3a5069a9ea4b153761caa5608fe
meir <contact@flamingo.dev>
2025-08-11 12:21:00 +0200

    allow importing files with no extension

--- src/resolver/mod.rs
+++ src/resolver/mod.rs
@@ -53,40 +53,42 @@ pub fn get_file(ctx: &mut Context, src: String) -> Result<Rc<RefCell<Resource>>,
     }) {
         Ok(rs.clone())
     } else {
-        if let Some(ext) = src.extension() {
-            match ext.to_str() {
-                Some("ds") => {
-                    let file = file::File::load_absolute(ctx, src.to_str().unwrap());
+        match src.extension().and_then(|ext| ext.to_str()) {
+            Some("ds") => {
+                let file = file::File::load_absolute(ctx, src.to_str().unwrap());
 
-                    let rc = Rc::new(RefCell::new(Resource::File(file)));
-                    ctx.resources.push(rc.clone());
-                    Ok(rc)
-                }
-                Some("scss") => {
-                    let content = std::fs::read_to_string(&src)
-                        .map_err(|_| format!("Failed to read SCSS file: {}", src.display()))?;
-                    let css = grass::from_string(content.clone(), &grass::Options::default())
-                        .map_err(|err| {
-                            format!("Failed to compile SCSS file: {}: {}", src.display(), err)
-                        });
+                let rc = Rc::new(RefCell::new(Resource::File(file)));
+                ctx.resources.push(rc.clone());
+                Ok(rc)
+            }
+            Some("scss") => {
+                let content = std::fs::read_to_string(&src)
+                    .map_err(|_| format!("Failed to read SCSS file: {}", src.display()))?;
+                let css = grass::from_string(content.clone(), &grass::Options::default()).map_err(
+                    |err| format!("Failed to compile SCSS file: {}: {}", src.display(), err),
+                );
 
-                    let name = src.file_stem().unwrap().to_str().unwrap();
-                    let mut hasher = DefaultHasher::new();
-                    content.hash(&mut hasher);
-                    let hash = hasher.finish();
-                    let path =
-                        Resource::get_output_path(ctx, format!("{}-{}.css", name, hash).as_str())
-                            .unwrap();
+                let name = src.file_stem().unwrap().to_str().unwrap();
+                let mut hasher = DefaultHasher::new();
+                content.hash(&mut hasher);
+                let hash = hasher.finish();
+                let path =
+                    Resource::get_output_path(ctx, format!("{}-{}.css", name, hash).as_str())
+                        .unwrap();
 
-                    let rc = Rc::new(RefCell::new(Resource::SCSS(
-                        src.to_str().unwrap().to_string(),
-                        path.to_str().unwrap().to_string(),
-                        css.unwrap(),
-                    )));
-                    ctx.resources.push(rc.clone());
-                    Ok(rc)
-                }
-                _ => {
+                let rc = Rc::new(RefCell::new(Resource::SCSS(
+                    src.to_str().unwrap().to_string(),
+                    path.to_str().unwrap().to_string(),
+                    css.unwrap(),
+                )));
+                ctx.resources.push(rc.clone());
+                Ok(rc)
+            }
+            _ => {
+                let with_ext = src.with_extension("ds");
+                if with_ext.exists() {
+                    get_file(ctx, with_ext.to_str().unwrap().to_string())
+                } else {
                     let relative_path =
                         Resource::get_relative_path_from_root(ctx, src.to_str().unwrap())
                             .map_err(|err| format!("Failed to get relative path: {}", err))?;
@@ -102,26 +104,6 @@ pub fn get_file(ctx: &mut Context, src: String) -> Result<Rc<RefCell<Resource>>,
                     ctx.resources.push(rc.clone());
                     Ok(rc)
                 }
-            }
-        } else {
-            let with_ext = src.with_extension("ds");
-            if with_ext.exists() {
-                get_file(ctx, with_ext.to_str().unwrap().to_string())
-            } else {
-                let relative_path =
-                    Resource::get_relative_path_from_root(ctx, src.to_str().unwrap())
-                        .map_err(|err| format!("Failed to get relative path: {}", err))?;
-
-                let output = Resource::get_output_path(ctx, &relative_path)
-                    .map_err(|err| format!("Failed to get output path: {}", err))?;
-
-                let rc = Rc::new(RefCell::new(Resource::Other(
-                    src.to_str().unwrap().to_string(),
-                    output.to_str().unwrap().to_string(),
-                )));
-
-                ctx.resources.push(rc.clone());
-                Ok(rc)
             }
         }
     }
