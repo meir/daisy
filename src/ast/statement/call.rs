be8b28b73d7e66e1760f6b1ac17cc5aa51e82940
meir <contact@flamingo.dev>
2025-08-19 10:05:51 +0200

    fixed statement calls

--- src/ast/statement/call.rs
+++ src/ast/statement/call.rs
@@ -6,11 +6,22 @@ use crate::ast::function::call_function;
 pub fn call(identifier: Expression, arguments: Vec<Expression>) -> Statement {
     Box::new(move |ctx, scope| {
         let function = identifier(ctx, scope);
-        if let Value::Function(..) = function {
-            call_function(ctx, &function, &arguments, scope);
-        } else {
-            panic!("Expected a function, got {}", function);
-        };
+        match function {
+            Value::Function(..) => {
+                call_function(ctx, &function, &arguments, scope);
+            }
+            Value::Scoped(inner_scope, value) => {
+                let mut inner_scope = inner_scope.clone();
+                let (func, vars, return_type, args) = value.try_into_function().unwrap();
+                call_function(
+                    ctx,
+                    &Value::Function(func, vars, return_type, args),
+                    &arguments,
+                    &mut inner_scope,
+                );
+            }
+            _ => panic!("Expected a function, got {}", function.get_type()),
+        }
 
         Result::NOP
     })
